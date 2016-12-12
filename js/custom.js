07311935e0a85c34af299c6d3e8c8e1e212167cc
TheilCamille <camille.theil@gmail.com>
2016-12-12 13:08:08 +0100

    Js new beta :3

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-//Apparition des horaires lors du passage du curseur sur l'icône Info
+/// Apparition des horaires lors du passage du curseur sur l'icône Info
 $(function(){
   $('#horaire').hide();
   $('#infohoraire').hover(function(){
@@ -6,20 +6,20 @@ $(function(){
   }, function(){
     $('#horaire').fadeOut();
   });
-});
+ });
 
 //Toggle acceuil
-$(function chngmain(){
-  function runEffect(){
-    $('link').attr('href', 'css/accrobranche.css');
-  };
+// $(function chngmain(){
+//  function runEffect(){
+//    $('link').attr('href', 'css/accrobranche.css');
+//  };
 
-  $('#accrobranche').on("click", function(){
-    $('main').effect("drop");
-    $('main').toggle("slow");
-    runEffect();
-  });
-});
+//  $('#accrobranche').on("click", function(){
+//    $('main').effect("drop");
+//    $('main').toggle("slow");
+//    runEffect();
+//  });
+// });
 
 
 // $(function test(){
@@ -49,3 +49,56 @@ $(function chngmain(){
 //     runEffect();
 //   });
 // });
+
+$( document ).ready(function() {
+    $('.mainaccro').hide();
+    $('.mainanni').hide();
+    $('.mainbist').hide();
+    $('.mainev').hide();
+    $('.mainskate').hide();
+});
+function onglet1() {
+
+  $('.mainaccro').hide();
+  $('.mainanni').hide();
+  $('.mainbist').hide();
+  $('.mainev').hide();
+  $('.mainskate').hide();
+
+    $('.mainac').effect('drop');
+    $('.mainac').fadeIn();
+
+    $('link').attr('href','./css/style.css');
+
+
+}
+
+function onglet2() {
+
+    $('.mainac').hide();
+    $('.mainanni').hide();
+    $('.mainbist').hide();
+    $('.mainev').hide();
+    $('.mainskate').hide();
+
+    $('.mainaccro').effect('drop');
+    $('.mainaccro').fadeIn();
+    $('link').attr('href','./css/accrobranche.css');
+}
+
+function onglet3() {
+
+  $('.mainac').hide();
+  $('.mainanni').hide();
+  $('.mainbist').hide();
+  $('.mainev').hide();
+  $('.mainaccro').hide();
+
+  $('.mainskate').effect('drop');
+  $('.mainskate').fadeIn();
+    $('link').attr('href','./css/skatepark.css');
+
+
+}
+
+function switchNav
